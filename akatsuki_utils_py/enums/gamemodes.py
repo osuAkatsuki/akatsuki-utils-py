96b157eb0a59a5b4eb765f8a377922472b79866c
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-07-20 15:00:22 +0200

    Add gamemodes enum

---
+++ akatsuki_utils_py/enums/gamemodes.py
@@ -0,0 +1,12 @@
+from enum import IntEnum
+
+class GameMode(IntEnum):
+    STANDARD = 0
+    TAIKO = 1
+    CTB = 2
+    MANIA = 3
+
+class SubMode(IntEnum):
+    VANILLA = 0
+    RELAX = 1
+    AUTOPILOT = 2
